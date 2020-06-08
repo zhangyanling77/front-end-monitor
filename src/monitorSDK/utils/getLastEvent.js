833d79b1ee7adcbf53a7f4fe9013a090daab0561
zhangyanling77 <18783226594@163.com>
2020-06-08 16:51:45 +0800

    fix: lastEvent should in global env

--- src/monitorSDK/utils/getLastEvent.js
+++ src/monitorSDK/utils/getLastEvent.js
@@ -3,15 +3,15 @@
  * @export
  * @returns
  */
-export default function () {
-  let lastEvent;
-  ['click', 'touchstart', 'mousedown', 'keydown', 'mouseover'].forEach(eventType => {
-    document.addEventListener(eventType, (event) => {
-      lastEvent = event;
-    }, {
-      capture: true, // 捕获阶段
-      passive: true // 默认不阻止默认事件
-    });
+let lastEvent;
+['click', 'touchstart', 'mousedown', 'keydown', 'mouseover'].forEach(eventType => {
+  document.addEventListener(eventType, (event) => {
+    lastEvent = event;
+  }, {
+    capture: true, // 捕获阶段
+    passive: true // 默认不阻止默认事件
   });
+});
+export default function () {
   return lastEvent;
 }
