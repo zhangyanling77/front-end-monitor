5a1be5cbb647f080423bdb85f10af001ff0cb512
zhangyanling77 <18783226594@163.com>
2020-06-09 17:18:35 +0800

    feat：

--- src/monitorSDK/lib/jsError.js
+++ src/monitorSDK/lib/jsError.js
@@ -8,12 +8,22 @@ function getLines(stack) {
 }
 
 export function injectJsError() {
-    // 监听全局未捕获的错误
-    window.addEventListener('error', function (event) { // 错误事件对象
-      console.log('event', event) 
-      let lastEvent = getLastEvent(); // 最后一个交互事件  
-      console.log('lastEvent', lastEvent)
-      // 脚本加载错误
+  // 监听全局未捕获的错误
+  window.addEventListener('error', function(event) { // 错误事件对象
+    console.log('event', event) 
+    let lastEvent = getLastEvent(); // 最后一个交互事件  
+    console.log('lastEvent', lastEvent)
+    // 说明是脚本资源加载错误 (如script, link等)
+    if (event.target && (event.target.src || event.target.href)) {
+      tracker.send({
+        kind: 'stability',
+        type: 'error',
+        errorType: 'resourceError', // js或css资源加载错误
+        filename: event.target.src || event.target.href,
+        tagName: event.target.tagName,
+        selector: getSelector(event.target)
+      })
+    } else {
       tracker.send({
         kind: 'stability',
         type: 'error',
@@ -24,5 +34,36 @@ export function injectJsError() {
         stack: getLines(event.error.stack),
         selector: lastEvent ? getSelector(lastEvent.path) : ''
       })
-    }, true);
+    }  
+  }, true);
+  // 当Promise被reject且没有reject处理器的时候，会触发unhandledrejection事件
+  // 这可能发生在window下，也可能发生在Worker中，对于调试回退错误处理很有用
+  window.addEventListener('unhandledrejection', function(event) {
+    // console.log(event)
+    let lastEvent = getLastEvent();
+    let message = '', filename = '', lineno = 0, colno = 0, stack = '';
+    const reason = event.reason;
+    if (typeof reason === 'string') {
+      message = reason
+    } else if (typeof reason === 'object') {
+      if (reason.stack) {
+         const matchResult = reason.stack.match(/at\s+(.+):(\d+):(\d+)/);
+         filename = matchResult[1]
+         lineno = matchResult[2]
+         colno = matchResult[3]
+         stack = getLines(reason.stack)
+      }   
+      message =  reason.message 
+    }
+    tracker.send({
+      kind: 'stability',
+      type: 'error',
+      errorType: 'promiseError',
+      message,
+      filename,
+      position: `${lineno}:${colno}`,
+      stack,
+      selector: lastEvent ? getSelector(lastEvent.path) : ''
+    })
+  }, true);
 }
