92926a625c605cc1eac92908b2ab0a0ebcc89900
zhangyanling <18783226594@163.com>
2020-06-11 22:15:56 +0800

    feat: performance

--- src/monitorSDK/lib/timing.js
+++ src/monitorSDK/lib/timing.js
@@ -1,7 +1,47 @@
 import tracker from '../utils/tracker';
 import onload from '../utils/onload';
+import getLastEvent from '../utils/getLastEvent';
+import getSelector from '../utils/getSelector';
 
 export function timing() {
+  let FMP, LCP;
+  // 增加一个性能条目的观察者
+  if (PerformanceObserver) {
+    new PerformanceObserver((entryList, observer) => {
+      let perfEntries = entryList.getEntries();
+      FMP = perfEntries[0]; 
+      observer.disconnect(); // 不再观察了
+    }).observe({ entryTypes: ['element'] }); // 观察页面中的意义的元素
+
+    new PerformanceObserver((entryList, observer) => {
+      let perfEntries = entryList.getEntries();
+      LCP = perfEntries[0];
+      observer.disconnect(); 
+    }).observe({ entryTypes: ['largest-contentful-paint'] }); // 观察页面中最大有意义的元素
+
+    new PerformanceObserver((entryList, observer) => {
+      let lastEvent = getLastEvent();
+      let firstInput = entryList.getEntries()[0];
+      console.log('FID', firstInput); // 首次输入延迟
+      if (firstInput) {
+        // processingStart 开始处理的时间 startTime 开点击的时间 差值就是处理的延迟
+        let inputDelay = firstInput.processingStart - firstInput.startTime;
+        let duration = firstInput.duration;//处理的耗时
+        if (inputDelay > 0 || duration > 0) {
+          tracker.send({
+            kind: 'experience', // 用户体验指标
+            type: 'firstInputDelay', // 首次输入延迟
+            inputDelay, // 延时的时间
+            duration, // 处理的时间
+            startTime: firstInput.startTime,
+            selector: lastEvent ? getSelector(lastEvent.path || lastEvent.target) : ''
+          });
+        }
+      }
+      observer.disconnect();
+    }).observe({ type: 'first-input', buffered: true });/ 
+  }
+  // 加载完执行
   onload(function() {
     setTimeout(function() {
       const {
@@ -17,6 +57,7 @@ export function timing() {
         domContentLoadedEventEnd,
         loadEventStart
       } = performance.timing;
+      // 发送每个阶段的耗时
       tracker.send({
         kind: 'experience', // 用户体验
         type: 'timing', // 统计每个阶段的时间
@@ -27,7 +68,24 @@ export function timing() {
         domContentLoadedTime: domContentLoadedEventEnd - domContentLoadedEventStart, // 
         timeToInteractive: domInteractive - fetchStart, // 首次DOM可交互时间
         loadTime: loadEventStart - fetchStart, // 完整的加载时间
-      })
+      });
+
+      // 发送性能指标
+      let FP = performance.getEntriesByName('first-paint')[0];
+      let FCP = performance.getEntriesByName('first-contentful-paint')[0];
+      console.log('FP', FP);
+      console.log('FCP', FCP);
+      console.log('FMP', FMP);
+      console.log('LCP', LCP);
+      tracker.send({
+        kind: 'experience',
+        type: 'paint',
+        firstPaint: FP.startTime,
+        firstContentfulPaint: FCP.startTime,
+        firstMeaningfulPaint: FMP.startTime,
+        largestContentfulPaint: LCP.startTime
+      });
+
     }, 3000)
   })
 }
